8aff0bc77e9ae7387699281b537b584d193bafdc
edji777 <49676371+edji777@users.noreply.github.com>
2020-04-17 12:28:16 +0300

    вносит исправления

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,61 @@
 'use strict';
 var toggles = document.querySelectorAll(".offers__toggle");
 var slides = document.querySelectorAll(".offers__list");
+var scroll = document.querySelector(".page-header__scroll");
 var phoneMask = IMask(document.getElementById('free-lesson-telephone'), {
   mask: '00000000000'
 });
 
 
-toggles.forEach(function (el, index) {
-  el.addEventListener("click", function () {
-    slides.forEach(function (el, index) {
-      slides[index].className = "offers__list offers-hide";
-      toggles[index].className = "offers__toggle";
-          })
-        slides[index].className = "offers__list offers-show";
-        toggles[index].className = "offers__toggle offers__toggle--active";
-        })
-      })
+var getToggles = function (toggle) {
+  toggle.forEach(function (el, index) {
+   el.addEventListener("click", function () {
+     slides.forEach(function (el, index) {
+       slides[index].className = "offers__list offers-hide";
+       toggles[index].className = "offers__toggle";
+           })
+         slides[index].className = "offers__list offers-show";
+         toggles[index].className = "offers__toggle offers__toggle--active";
+         })
+       })
+     }
+
+getToggles(toggles);
+
+// var getToggles = function (toggle, slides) {
+//   toggle.forEach(function (el, index) {
+//    el.addEventListener("click", function () {
+//      var getSlides = function (slides) {
+//      slide.forEach(function (el, index) {
+//        slides[index].className = "offers__list offers-hide";
+//        toggles[index].className = "offers__toggle";
+//            })
+//          slides[index].className = "offers__list offers-show";
+//          toggles[index].className = "offers__toggle offers__toggle--active";
+//        }
+//          })
+//        })
+//      }
+//
+
+// getSlides(slide);
+//  toggles.forEach(function (el, index) {
+//   el.addEventListener("click", function () {
+//     slides.forEach(function (el, index) {
+//       slides[index].className = "offers__list offers-hide";
+//       toggles[index].className = "offers__toggle";
+//           })
+//         slides[index].className = "offers__list offers-show";
+//         toggles[index].className = "offers__toggle offers__toggle--active";
+//         })
+//       })
+
+$(scroll).click(function () {
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 500);
+  return false;
+});
 
 jQuery(document).ready(function($){
   $('.instructors__list').slick({
