2569ab2aff2da04c0f62ae4719b7e850639f0f6f
edji777 <49676371+edji777@users.noreply.github.com>
2020-04-20 09:43:43 +0300

    вносит требуемые исправления

--- source/js/script.js
+++ source/js/script.js
@@ -1,55 +1,22 @@
 'use strict';
-var toggles = document.querySelectorAll(".offers__toggle");
-var slides = document.querySelectorAll(".offers__list");
-var phoneMask = IMask(document.getElementById('free-lesson-telephone'), {
-  mask: '00000000000'
-});
+var toggles = document.querySelectorAll('.offers__toggle');
+var slides = document.querySelectorAll('.offers__list');
 
-
-toggles.forEach(function (el, index) {
-  el.addEventListener("click", function () {
-    slides.forEach(function (el, index) {
-      slides[index].className = "offers__list offers-hide";
-      toggles[index].className = "offers__toggle";
-          })
-        slides[index].className = "offers__list offers-show";
-        toggles[index].className = "offers__toggle offers__toggle--active";
-        })
-      })
-
-jQuery(document).ready(function($){
-  $('.instructors__list').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          variableWidth: true,
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          variableWidth: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 2
-        }
-      }
-    ]
+var getSlides = function (slide) {
+  slide.forEach(function (el, index) {
+    slide[index].className = 'offers__list offers-hide';
+    toggles[index].className = 'offers__toggle';
   });
-});
+};
 
-jQuery(document).ready(function($){
-  $('.feedback__list').slick({
-    variableWidth: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
+var getToggles = function (toggle) {
+  toggle.forEach(function (element, index) {
+    element.addEventListener('click', function () {
+      getSlides(slides);
+      slides[index].className = 'offers__list offers-show';
+      toggles[index].className = 'offers__toggle offers__toggle--active';
+    });
   });
-});
+};
+
+getToggles(toggles);
